3bd194e009d6f4c56b1f48d10d2857f5d6454baf
BaoPQ19 <baopq19@gmail.com>
2021-06-28 19:04:04 +0700

    add assets folder

---
+++ src/components/Model.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Model extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <img src="./images/model.jpg"></img>
+                </div>
+                <div className="col-12 col-md-6">
+                </div>
+            </div>
+        )
+    }
+}
